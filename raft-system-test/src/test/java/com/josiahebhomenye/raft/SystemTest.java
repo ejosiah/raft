1c0a33a2a42bc795cb0f0e48fc45a34b8e34b691
Josiah Ebhomenye <josiah.ebhomenye@news.co.uk>
2019-12-18 08:26:01 +0000

    system test in progress

---
+++ raft-system-test/src/test/java/com/josiahebhomenye/raft/SystemTest.java
@@ -0,0 +1,13 @@
+package com.josiahebhomenye.raft;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SystemTest {
+
+    @Test
+    public void validate_that_the_system_meets_the_raft_guarantees(){
+        fail("Not yet implemented!");
+    }
+}
